344ce9a3890db1cccb1e5a6659ffc4c328d92ede
RaphBoutch <raphalou8@hotmail.com>
2025-11-24 12:21:39 -0500

    Minuteur une minute

--- src/main.cpp
+++ src/main.cpp
@@ -56,8 +56,8 @@ int positionX=20;
 int positionY=720;
 
 //Les recu par comm
-int positionXRecu=0;
-int positionYRecu=0;
+int positionXRecu=43;
+int positionYRecu=21;
 int flagBleuRecu=0;
 int etatJeuRecu=0;
 
@@ -73,7 +73,8 @@ int etatJeuRecu=0;
  * 3=Terminé car deux bumper ON
 ******************************************************************************************/
 void setEtatJeu(){
-if((positionX!=0 || positionY!=0) && flagBumper==0){etatJeu=1; debutJeu=millis();}
+if((positionX!=0 || positionY!=0) && flagBumper==0 && debutJeu==0){etatJeu=1; debutJeu=millis();}
+if((positionX!=0 || positionY!=0) && flagBumper==0){etatJeu=1;}
 if((positionX!=0 || positionY!=0) && flagBumper==1){etatJeu=2;}
 if((positionX!=0 || positionY!=0) && flagBumper==1 && etatJeuRecu==2){etatJeu=3;}
 if(millis()-debutJeu>60000){etatJeu=3;}
@@ -345,76 +346,7 @@ Auteur: Samuel B. Manelli
 Description: Permet le contrôle du robot à l'aide d'une manette. Reçois des
 valeurs via le esp32
 */
-void deplacementmanette()
-{
-    float speedAvance=float(manette[0])/100.0;
-    float speedRecule=float(manette[1])/100.0;
-    float ratioD=1;
-    float ratioG=1;
-    float vitesseDroite;
-    float vitesseGauche;
-    // Faire un stop ou une priorité sur avance et recule pour éviter qu'il ne soit appuyer en même temps
-    // l'idée est que si les deux sont différent de 0 en même temps le robot stop
-    if (manette[0] != 0 && manette[1] != 0)
-    {
-        MOTOR_SetSpeed(DROITE, 0);
-        MOTOR_SetSpeed(GAUCHE, 0);
-    }
-    else
-    {
-        //permet de tourner a gauche
-        if (manette[2] < 45 )
-        {
-            ratioG = (manette[2] / 25);
-        }
 
-        // permet de tourner a droite
-        if (manette[2] > 55)
-        {
-            ratioD = ((100-manette[2]) / 25);
-        }
-
-        if (manette[2] > 45 && manette[2] < 55)
-        {
-            ratioD = 1;
-            ratioG = 1;
-            //mettre un interval au lieu d'une valeur fixe pour contre le driff
-        }
-
-       // permet de limiter la vitesse max
-        if (speedAvance > 0.5 )
-        {
-            speedAvance = maxSpeedAvance;
-        }
-        //permet de limiter la vitesse de reculons max
-        if (speedRecule < -0.5)
-        {
-            speedRecule = maxSpeedRecule;
-        }
-        if (speedAvance<speedRecule){
-            vitesseDroite=speedRecule * ratioD/-1;
-            vitesseGauche=speedRecule * ratioG/-1;
-        }
-         else{
-            vitesseDroite = speedAvance * ratioD;
-            vitesseGauche= speedAvance * ratioG;
-        }
-        if (vitesseDroite<0.15 && vitesseDroite > 0){
-            vitesseDroite=0.2;
-        }
-        if (vitesseGauche<0.15 && vitesseGauche > 0){
-            vitesseGauche=0.2;
-        }
-        if (vitesseDroite>-0.15 && vitesseDroite < 0){
-            vitesseDroite=-0.2;
-        }
-        if (vitesseGauche>-0.15 && vitesseGauche < 0){
-            vitesseGauche=-0.2;
-        }
-        MOTOR_SetSpeed(DROITE, vitesseDroite);
-        MOTOR_SetSpeed(GAUCHE, vitesseGauche);
-    }
-}
 //set speed pour faire deplacer le robot
 // MOTOR_SetSpeed(DROITE,speedAvance*ratioD);
 // MOTOR_SetSpeed(GAUCHE,speedAvance*ratioG);
